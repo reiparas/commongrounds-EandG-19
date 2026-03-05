a38da1d5f56a366e66b78ffca63f5507186053de
RandomStuffMaker2 <haagen.padron-hansen@student.ateneo.edu>
2026-03-06 04:35:17 +0800

    Implemented bookclub app with appropriate models, views, and templates

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = [ "name" ]
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    author = models.CharField()
+    publication_year = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-publication_year"]
+
+    def __str__(self):
+        return self.title
+    
--- bookclub/views.py
+++ bookclub/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookclub/book_list.html", {"books": books})
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    return render(request, "bookclub/book_detail.html", {"book": book})
