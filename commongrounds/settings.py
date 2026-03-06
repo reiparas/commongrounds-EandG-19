5bf7ba213a92c2b7b9fc797718fa2de00ec14ee1
omegadzilla <frederick.filovenia@gmail.com>
2026-03-06 21:26:34 +0800

    Updated to hide secret key, and include 'diyprojects' in apps

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--wtqlejujotdwb(b3y^1&&)$!8=tkpm^c40_o4p)&du369s)gx'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diyprojects'
 ]
 
 MIDDLEWARE = [
