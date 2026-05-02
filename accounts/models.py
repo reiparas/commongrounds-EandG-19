7c90912bc6a20d75c87cad940c8ff045433953fb
omegadzilla <frederick.filovenia@gmail.com>
2026-05-02 14:51:29 +0800

    Added basic user management and respective templates

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Profile(models.model):
+class Profile(models.Model):
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
--- commongrounds/urls.py
+++ commongrounds/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('diyprojects/', include('diyprojects.urls', namespace="diyprojects")),
     path('admin/', admin.site.urls),
     path('commissions/', include('commissions.urls')),
