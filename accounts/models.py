006d60792c8605be0b451b1d126fa863f146a902
omegadzilla <frederick.filovenia@gmail.com>
2026-05-02 13:24:10 +0800

    Account App made and Profile model added

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Profile(models.model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    displayName = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
