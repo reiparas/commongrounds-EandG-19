17adcd8fd807eb52a90a2edd4183b8c66e32a8a5
omegadzilla <frederick.filovenia@gmail.com>
2026-05-04 23:28:44 +0800

    Fixed URL for project details and updated templates to make use of base

--- diyprojects/urls.py
+++ diyprojects/urls.py
@@ -3,7 +3,7 @@ from .views import project_list, project_detail
 
 urlpatterns = [
     path('projects', project_list, name='project_list'),
-    path('project<int:id>', project_detail, name='project_detail')
+    path('project/<int:id>', project_detail, name='project_detail')
 ]
 
 app_name = "diyprojects"
